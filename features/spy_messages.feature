5601af98422096c6538f6f00a45e5095e5a96a90
hector <hectorjadame213@gmail.com>
2024-12-09 14:08:16 -0700

    testing CICD behave

--- features/spy_messages.feature
+++ features/spy_messages.feature
@@ -4,9 +4,19 @@ I should be able to encode and decode messages
 So that I can chat with my spy friends like a pro.
 
 Scenario: I can successfully encode a secret message
-    Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
-    # write your steps here
+Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
+When I click on the "decoder-setting" and switch the value to "E"
+and I click on the "shift-amount" and switch the value to "1"
+and I click on the "letters" and enter "hi"
+and I click "submit" button
+Then I should see "ij" in the "decoded_message" 
+
 
 Scenario: I can successfully decode a secret message
-    Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
-    # write your steps here
+Given I open the url "https://www.hanginghyena.com/solvers_a/caesar-cipher-decoder"
+When I click on the "decoder-setting" and switch the value to "D"
+and I click on the "shift-amount" and switch the value to "1"
+and I click on the "letters" and enter "ij"
+and I click "submit" button
+Then I should see "hi" in the "decoded_message"
+    
--- features/steps/builtin_steps.py
+++ features/steps/builtin_steps.py
@@ -1,1 +1,39 @@
-from behave_webdriver.steps import * # ignore
+from behave_webdriver.steps import *  # ignore
+from behave import when, then, given
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select  # Import the Select class
+import time
+
+@given('I open the url "{url}"')
+def step_go_to_url(context, url):
+    context.behave_driver.get(url)
+    time.sleep(2)
+
+@when('I click on the "{selector}" and switch the value to "{value}"')
+def step_click_on_select_and_enter_value(context, selector, value):
+    s= context.behave_driver.find_element(By.ID, selector)
+    select = Select(s)
+    select.select_by_value(value)
+
+@when('I click on the "{textId}" and enter "{value}"')
+def step_to_enter_text_into_text_box(context, textId, value):
+    text_area = context.behave_driver.find_element(By.ID, textId)
+    text_area.clear()
+    text_area.send_keys(value)
+
+@when('I click "{btnId}" button')
+def step_to_click_btn(context, btnId):
+    btn = context.behave_driver.find_element(By.ID, btnId)
+    btn.click()
+    time.sleep(3)
+@then('I should see "{value}" in the "{div}"')
+def step_to_verify(context, value, div):
+    div_container = context.behave_driver.find_element(By.ID, div)
+    p = div_container.find_element(By.TAG_NAME, "p")
+    assert p.text == value, f"Expected '{value}', but got '{p.text}'"
+
+
+
+
+
+
